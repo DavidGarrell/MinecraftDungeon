b8db8f94d2367b66b5cbfdbcc70b86ffa17fcc82
David Garrell <119519280+DavidGarrell@users.noreply.github.com>
2026-03-06 18:55:57 +0100

    Split enchants into dedicated effect classes and localize visuals

---
+++ src/main/java/de/fleaqx/minecraftDungeons/enchant/effect/CriticalEnchantEffect.java
@@ -0,0 +1,33 @@
+package de.fleaqx.minecraftDungeons.enchant.effect;
+
+import de.fleaqx.minecraftDungeons.enchant.EnchantDefinition;
+import de.fleaqx.minecraftDungeons.enchant.EnchantService;
+import de.fleaqx.minecraftDungeons.runtime.DamageIndicatorService;
+import de.fleaqx.minecraftDungeons.runtime.DungeonService;
+import org.bukkit.entity.LivingEntity;
+import org.bukkit.entity.Player;
+
+import java.math.BigInteger;
+
+public final class CriticalEnchantEffect extends BaseEnchantEffect {
+
+    public CriticalEnchantEffect() {
+        super("critical_enchant");
+    }
+
+    @Override
+    public BigInteger extraDamage(Player player, LivingEntity target, BigInteger swordDamage, EnchantDefinition definition, EnchantService service) {
+        return super.extraDamage(player, target, swordDamage, definition, service);
+    }
+
+    @Override
+    public void applyPostHit(Player player,
+                             LivingEntity mainTarget,
+                             BigInteger swordDamage,
+                             EnchantDefinition definition,
+                             EnchantService service,
+                             DungeonService dungeonService,
+                             DamageIndicatorService indicatorService) {
+        super.applyPostHit(player, mainTarget, swordDamage, definition, service, dungeonService, indicatorService);
+    }
+}
